a0963fcce6a5e48d914bf173229f7fc88d9efce3
jramirez@chicagotechacademy.org <jramirez@chicagotechacademy.org>
2013-12-06 15:51:00 -0600

    homework-mr.rush

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,25 @@ describe 'Calculate class' do
 		@calculate = Calculator.new
 		@calculate.calculate(1).should == 1
 	end
+
+
+    it 'returns 2 from 2' do
+    	@calculate = Calculator.new
+		@calculate.calculate(2).should == 2
+    end
+
+    it 'returns string fizz when it receives 3' do
+    	@calculate = Calculator.new
+		@calculate.calculate(3).should == "fizz"
+	end
+
+    it 'returns 4 from 4' do
+    	@calculate = Calculator.new
+		@calculate.calculate(4).should == 4
+		end
+
+    it 'returns buzz from 5' do
+    	@calculate = Calculator.new
+		@calculate.calculate(5).should == "buzz"
+		end
 end
